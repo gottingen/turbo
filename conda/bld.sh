3e22bb721dcf2fb0a1fe0f5f30ba3539cc3c88e9
lijippy@163.com <lijippy@163.com>
2023-01-09 07:28:34 +0800

    build sh

--- conda/bld.sh
+++ conda/bld.sh
@@ -5,9 +5,9 @@ mkdir build
 cd build
 cmake .. -DCMAKE_INSTALL_PREFIX=$PREFIX \
         -DCMAKE_BUILD_TYPE=Release \
-        -DBUILD_UNIT_TESTS=OFF \
-        -DBUILD_EXAMPLES=OFF \
-        -DBUILD_BENCHMARK=OFF
+        -DENABLE_TESTING=OFF \
+        -DENABLE_BENCHMARK=OFF \
+        -DENABLE_EXAMPLE=OFF
 
 cmake --build .
 cmake --build . --target install
