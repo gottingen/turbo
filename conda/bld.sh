e63046b7e1c428311512e788197ff47d139da714
lijippy@163.com <vvmulanwang@gmail.com>
2023-04-01 18:37:25 +0800

    using unicode for all

--- conda/bld.sh
+++ conda/bld.sh
@@ -7,6 +7,7 @@ cmake .. -DCMAKE_INSTALL_PREFIX=$PREFIX \
         -DCMAKE_BUILD_TYPE=Release \
         -DTURBO_BUILD_TESTING=OFF \
         -DCMAKE_INSTALL_LIBDIR=lib \
+        -DTURBO_BUILD_EXAMPLE=OFF \
         -DBUILD_SHARED_LIBS=ON
 
 cmake --build .
