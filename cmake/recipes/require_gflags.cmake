44fb586a4ca11e2cedecbc59e0a11bb8bf34d391
Jeff.li <lijippy@163.com>
2023-06-21 04:47:49 +0800

    update carbin deps

--- cmake/recipes/require_gflags.cmake
+++
@@ -1,8 +0,0 @@
-
-
-find_path(GFLAGS_INCLUDE_PATH gflags/gflags.h)
-find_library(GFLAGS_LIBRARY NAMES gflags libgflags)
-if((NOT GFLAGS_INCLUDE_PATH) OR (NOT GFLAGS_LIBRARY))
-    message(FATAL_ERROR "Fail to find gflags")
-endif()
-include_directories(${GFLAGS_INCLUDE_PATH})
