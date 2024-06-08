925526945ffd92f6bce571cde832acfb162f93bb
jeff.li <lijippy@163.com>
2024-06-08 11:52:24 +0800

    add caches

---
+++ turbo/container/cache.h
@@ -0,0 +1,25 @@
+//
+// Copyright (C) 2024 EA group inc.
+// Author: Jeff.li lijippy@163.com
+// All rights reserved.
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+//
+// Created by jeff on 24-6-8.
+//
+
+#ifndef TURBO_CACHE_H
+#define TURBO_CACHE_H
+
+#endif //TURBO_CACHE_H
